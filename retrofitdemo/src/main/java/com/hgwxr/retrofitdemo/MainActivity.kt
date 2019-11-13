f14f46e2a1420592dfaf735dd494a4dd231406cc
long.wang <github@xx.com>
2019-11-13 10:08:27 +0800

    Initial commit

---
+++ retrofitdemo/src/main/java/com/hgwxr/retrofitdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hgwxr.retrofitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetrofitDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit_demo)
+    }
+}
